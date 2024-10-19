0c6be4031c9e7ac6e92235e75b75adbfccd40c33
Moomba Michelo Muloba <moomba.muloba@news.co.uk>
2024-10-19 20:46:04 +0100

    create our first EC2 instance
    - move the provider to a region where the ami exists
    - add gitignore
    - initialize terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 }
