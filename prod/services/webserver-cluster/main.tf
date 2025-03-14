a9eea482be00d819e1923c7b611db23d49078249
Moomba Michelo Muloba <moomba.muloba@news.co.uk>
2025-03-14 12:13:46 +0000

    chore: set default tags example

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -14,6 +14,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Owner     = "team-foo"
+      ManagedBy = "Terraform"
+    }
+  }
 }
 
 module "webserver-cluster" {
