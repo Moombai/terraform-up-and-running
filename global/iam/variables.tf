02f9fde14d17f65b63e2eecdb2e5c613b41ef3ae
Moomba Michelo Muloba <moomba.muloba@news.co.uk>
2025-03-21 11:44:17 +0000

    chore: loops, expressions and directives

--- global/iam/variables.tf
+++ global/iam/variables.tf
@@ -3,3 +3,33 @@ variable "user_names" {
   type        = list(string)
   default     = ["neo", "trinity", "morpheus"]
 }
+
+output "upper_user_names" {
+  value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type        = map(string)
+  default = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "mentor"
+  }
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = { for name, role in var.hero_thousand_faces : upper(name) => upper(role) }
+}
+
+output "for_directive" {
+  value = "%{for name in var.user_names}${name}, %{endfor}"
+}
+
+output "for_directive_index" {
+  value = "%{for i, name in var.user_names}(${i}) ${name}, %{endfor}"
+}
